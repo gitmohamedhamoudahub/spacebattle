42a811a8dddb807a18b2c9a23c8ac69ffb904d11
gitmohamedhamoudahub <eng.mohamed.hobby@gmail.com>
2024-12-30 08:24:14 -0500

    Removing Class SpaceShip

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ class Battle{
         this.fighter2 = fighter2;
         this.round = 1;
     }
-    battleStart = function battleStart(){
+    battleStart(){
         console.log("battle Started" );
-        console.log(this.fighter1);
-        console.log(this.fighter2);
+        console.log(`${this.fighter1.name} vs ${this.fighter2.name}`);
+    }
+    fight() {
+        console.log(`Round ${this.round}:`);
     }
 }
 class Fighter {
@@ -18,12 +20,17 @@ class Fighter {
         this.attackPower = attackPower;
         this.defensePower = defensePower;
             }
-        attack = function attack(weapon) {
+        attack(target,weapon) {
             this.weapon = weapon;
-                console.log(`Attack with ` + weapon);
+            this.target = target;
+            const attackEffect = Math.max(this.attackPower - target.defensePower, 0);
+            console.log(`Attack with ` + weapon);
+            console.log(`Attack Effect  ` + attackEffect);
+            target.health -= attackEffect;
+            console.log(`${this.name} attacks ${target.name}, affected with ${attackEffect} `);
             
             } 
-        defend = function defend(){
+        defend(){
             console.log(`Defend with `);
         }        
             // defend : () => {
@@ -32,45 +39,44 @@ class Fighter {
     
 }
 
-class SpaceShip{
-    constructor(name,hull,firePower,accuracy,commander){
-        this.name = name;
-        this.hull = hull;
-        this.firePower = firePower;
-        this.accuracy = accuracy;
-        this.commander = commander;         //Alien / Captain
+// class SpaceShip{
+//     constructor(name,hull,firePower,accuracy,commander){
+//         this.name = name;
+//         this.hull = hull;
+//         this.firePower = firePower;
+//         this.accuracy = accuracy;
+//         this.commander = commander;         //Alien / Captain
        
-    }
+//     }
 
-    attack = function(weapon) {
-        console.log(`Fire on ` + weapon);
-    }
-    defend = () => {
-        console.log(`Defend on`);
-    }
-    attackBack = function(weapon)  {
-        console.log(`Fire back` + weapon);
-        this.weapon = weapon;
-    }
-    retreat = function()  {
+//     attack(weapon) {
+//         console.log(`Fire on ` + weapon);
+//     }
+//     defend()  {
+//         console.log(`Defend on`);
+//     }
+//     attackBack(weapon)  {
+//         console.log(`Fire back` + weapon);
+//         this.weapon = weapon;
+//     }
+//     retreat = function()  {
     
-    }
-}
+//     }
+// }
+
+
 let captainFighter = new Fighter('Captain Mohamed',11,12,13);
 console.log("Fighter " + captainFighter.name)
-captainFighter.attack('laser');
-
-let usSpaceShip = new SpaceShip('USS Space',11,12,13,captainFighter);
-usSpaceShip.attack(captainFighter.weapon); 
-usSpaceShip.defend();
-usSpaceShip.attackBack(captainFighter.weapon);
 
 let alienFighter = new Fighter('Alien Z1',11,12,13);
 
-let alienSpaceShip = new SpaceShip('Alien 1',11,12,13,alienFighter);
-alienFighter.attack('Laser');
-alienSpaceShip.attack(alienFighter.weapon);
 
 let battle = new Battle(captainFighter,alienFighter);
 battle.battleStart();
-battle.fighter1.attack('Laser 1');
+
+function attack()
+{
+    let txtgame = document.querySelector('.txtGame');
+    // console.log(txtgame);
+    txtgame.textContent += 'Attack\n '; 
+}
