758973da9c1650e3fe3f7dfc7a4a54c589d40dd7
gitmohamedhamoudahub <eng.mohamed.hobby@gmail.com>
2025-01-02 07:48:18 -0500

    Adjusting attack effect

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ class Battle{
         this.fighter2 = fighter2;
         this.round = 1;
     }
+    
     startBattle(){
         console.log("battle Started" );
         console.log(`${this.fighter1.name} vs ${this.fighter2.name}`);
     }
     fight() {
         console.log(`Fighting In Round ${this.round}:`);
-        const attackEffect = Math.max(this.fighter1.attackPower - this.fighter2.defensePower, 0);
-        this.fighter2.health -= attackEffect;
+        // const attackEffect = Math.max(this.fighter1.firePower - this.fighter2.accuracy, 0);
+        // this.fighter2.accuracy -= attackEffect;
         updateDataBind(); 
 
     }
@@ -22,19 +23,30 @@ class Battle{
 class Fighter {
     constructor(name,health,attackPower,defensePower) {
         this.name = name;
-        this.health = health;
-        this.attackPower = attackPower;
-        this.defensePower = defensePower;
+        this.hull = health;
+        this.firePower = attackPower;
+        this.accuracy = defensePower;
             }
-        attack(target,weapon) {
+        attack(target ,weapon) {
             this.weapon = weapon;
             this.target = target;
-            const attackEffect = Math.max(this.attackPower - target.defensePower, 0);
+            const attackEffect = this.firePower ;
             console.log(`Attack with ` + weapon);
             console.log(`Attack Effect  ` + attackEffect);
-            target.health -= attackEffect;
             console.log(`${this.name} attacks ${target.name}, affected with ${attackEffect} `);
+            let random = Math.random();
+            console.log(`Random vs Accuracy  ` + random + ' - ' + this.accuracy);
             
+            if (random < this.accuracy) {
+                    console.log('You have been hit! ' + attackEffect);
+                    target.hull -= attackEffect;
+            
+            }
+            else
+            {
+                console.log('You have been missed the!' + target.name);
+            }
+            return target;
             } 
         defend(){
             console.log(`Defend with `);
@@ -48,31 +60,36 @@ function updateDataBind(){
     alienDiv.innerHTML = `
     <div>
     <h3>${alienFighter.name} </h3>
-    <div>Health: ${alienFighter.health} </div>
-    <div>Attack Power: ${alienFighter.attackPower} </div>
-    <div> Defense Power: ${alienFighter.defensePower} </div>
+    <hr />
+    <div>Health: ${alienFighter.hull} </div>
+    <div>Attack Power: ${alienFighter.firePower} </div>
+    <div> Defense Power: ${alienFighter.accuracy} </div>
 </div>
     `;
 
 playerDiv.innerHTML = `
 <div>    
 <h3>${captainFighter.name} </h3>
-    <div> Health: ${captainFighter.health} </div>
-    <div> Attack Power: ${captainFighter.attackPower} </div>
-    <div> Defense Power: ${captainFighter.defensePower} </div>
+<hr />
+    <div> Health: ${captainFighter.hull} </div>
+    <div> Attack Power: ${captainFighter.firePower} </div>
+    <div> Defense Power: ${captainFighter.accuracy} </div>
 </div>
 `;
 }
 
-let captainFighter = new Fighter('Captain Mohamed',20,5,2);
-let alienFighter = new Fighter('Alien Z1',15,4,1);
+let captainFighter = new Fighter('Captain Mohamed',20,5,0.2);
+let alienFighter = new Fighter('Alien Z1',15,4,0.1);
 
 let battle = new Battle(captainFighter,alienFighter);
 battle.startBattle();
 battle.fight();
-updateDataBind();
+
 function attack()
 {
     let txtgame = document.querySelector('.txtGame');
-    txtgame.textContent += 'Attack\n '; 
+    txtgame.textContent += 'Attack\n ';
+    
+    captainFighter.attack(alienFighter,'Laser');
+    updateDataBind();
 }
